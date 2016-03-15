cd07c8719d4a49ef5fca1bdb8fedcdbf226c9a05
Thomas Schell <thom.schell@gmail.com>
2016-03-15 14:25:47 +0100

    added auth tests

--- app/src/reducers/auth.js
+++ app/src/reducers/auth.js
@@ -1,7 +1,7 @@
 import { SIGNIN_SUCCESS } from '../actions/signIn'
 import { SIGNOUT_SUCCESS } from '../actions/signOut'
 
-const auth = (state = { loggedin: false, token: null }, action) => {
+const auth = (state = { loggedIn: false, token: null }, action) => {
   switch (action.type) {
     case SIGNIN_SUCCESS:
       return { loggedIn: true, token: action.payload }
