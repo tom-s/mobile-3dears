60c8acb56c220f7930975e0f235f571e58ade4d7
Thomas Schell <thom.schell@gmail.com>
2016-04-24 01:39:12 +0200

    added redirect after login

--- app/src/sagas/signIn.js
+++ app/src/sagas/signIn.js
@@ -1,10 +1,11 @@
-import { call, put } from 'redux-saga/effects'
+import { call, put, select } from 'redux-saga/effects'
 import { push } from 'react-router-redux'
 import { takeEvery } from 'redux-saga'
 import { SIGNIN_REQUEST, SIGNIN_ERROR, SIGNIN_SUCCESS } from '../actions/signIn'
 import { signInApi } from '../services/api'
 import { setAuthToken } from '../services/auth'
 import { NOTIFY_ERROR, NOTIFY_SUCCESS } from '../actions/notification'
+import { getReferer } from '../selectors/url'
 
 export function * loginSaga ({ payload }) {
   const { email, password } = payload
@@ -13,7 +14,9 @@ export function * loginSaga ({ payload }) {
     setAuthToken(token)
     yield put({ type: SIGNIN_SUCCESS, payload: token })
     yield put({ type: NOTIFY_SUCCESS, payload: 'You are now logged in' })
-    yield put(push('/'))
+    const state = yield select()
+    const referer = getReferer(state)
+    yield put(push(referer || '/'))
   } catch (error) {
     yield put({ type: SIGNIN_ERROR })
     yield put({ type: NOTIFY_ERROR, payload: 'Invalid username/password pair' })
--- app/src/sagas/url.js
+++ app/src/sagas/url.js
@@ -3,18 +3,20 @@ import { push } from 'react-router-redux'
 import { takeEvery } from 'redux-saga'
 import { fork } from 'redux-saga/effects'
 import { URL_ERROR, URL_AUTH_ERROR, URL_SAVE_REFERER } from '../actions/url'
-import { NOTIFY_ERROR, NOTIFY_SUCCESS } from '../actions/notification'
+import { NOTIFY_ERROR } from '../actions/notification'
 
 export function * urlErrorSaga ({ payload }) {
   const { referer } = payload
   yield put({ type: URL_SAVE_REFERER, payload: referer })
-  yield put(push('/sign_in')) // redirect to sign in
+  yield put(push('/')) // redirect to sign in
+  yield put({ type: NOTIFY_ERROR, payload: 'This page does not exist, try to access another page' })
 }
 
 export function * urlAuthErrorSaga ({ payload }) {
   const { referer } = payload
   yield put({ type: URL_SAVE_REFERER, payload: referer })
   yield put(push('/sign_in')) // redirect to sign in
+  yield put({ type: NOTIFY_ERROR, payload: 'You need to log in to access this page' })
 }
 
 function * watchUrlError () {
